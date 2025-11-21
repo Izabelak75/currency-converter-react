c83a10b266a7c6b0002816fae1f519e8a4b16380
Izabela Kuc <isabela.kuc@gmail.com>
2025-11-21 01:34:10 -0800

    currenncy conventor with interactions

---
+++ src/App/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+  {
+    short: "EUR",
+    name: "Euro",
+    rate: 4.29,
+  },
+  {
+    short: "USD",
+    name: "Dolar amerykański",
+    rate: 3.73,
+  },
+  {
+    short: "CAD",
+    name: "Dolar kanadyjski",
+    rate: 2.65,
+  },
+];
