71e8b277f426fad7d797062743638619d4cf97b4
Izabela Kuc <isabela.kuc@gmail.com>
2025-11-21 22:18:21 -0800

    replaced incorecct parenthes:(( with braces {}

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -1,8 +1,8 @@
 import "./style.css";
 
-export const Result = ({ result }) => {
+export const Result = ({ result }) => (
   <p className="result">
-    {result !== undefined && (
+    {!!result && (
       <>
         {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;={" "}
         <strong>
@@ -10,5 +10,5 @@ export const Result = ({ result }) => {
         </strong>
       </>
     )}
-  </p>;
-};
+  </p>
+);
