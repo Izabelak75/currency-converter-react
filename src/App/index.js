43f2b5c4f41c47d91d3de87ba7e09aa626fef326
Izabela Kuc <isabela.kuc@gmail.com>
2025-11-23 00:18:22 -0800

    Updated index.js in App

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import { Form } from "./Form";
+import { Clock } from "./Clock";
 import { currencies } from "./currencies";
 
 function App() {
   return (
     <div className="app">
+      <Clock />
       <Form />
     </div>
   );
