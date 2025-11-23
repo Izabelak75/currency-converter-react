b713aa5754fe16f6566bdf7a173e6cf624c2d3c7
Izabela Kuc <isabela.kuc@gmail.com>
2025-11-22 22:47:41 -0800

    removed resul from App and added to Form

--- src/App/index.js
+++ src/App/index.js
@@ -4,21 +4,9 @@ import { Form } from "./Form";
 import { currencies } from "./currencies";
 
 function App() {
-  const [result, setResult] = useState();
-
-  const calculateResult = (currency, amount) => {
-    const rate = currencies.find(({ short }) => short === currency).rate;
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency,
-    });
-  };
-
   return (
     <div className="app">
-      <Form result={result} calculateResult={calculateResult} />
+      <Form />
     </div>
   );
 }
