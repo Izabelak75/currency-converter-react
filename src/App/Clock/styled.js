afb83e0ffe0f831a629b52a0d7a865d6dfd19bf5
Izabela Kuc <isabela.kuc@gmail.com>
2025-11-24 22:34:41 -0800

    add styled.js for styled components Clock Form Result

---
+++ src/App/Clock/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  text-align: right;
+  font-family: monospace;
+  color: ${({ theme }) => theme.color.boulder};
+  margin-top: 10px;
+  scroll-margin-right: 10 px;
+`;
