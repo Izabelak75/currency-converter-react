e7f73e8701190321e0fde54579c18ddc1d87ab44
Izabela Kuc <isabela.kuc@gmail.com>
2025-11-24 21:45:28 -0800

    added new component

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  color: {
+    black: "#000",
+    boulder: "#777",
+    silver: "#ccc",
+    flax: "#eed187",
+    white: "#fff",
+    ebb: "#e9e4e4",
+    forestGreen: "#386b22",
+    indigo: " #4e39aa",
+    red: "#f71605a6",
+    purple: "#a410e9",
+  },
+};
