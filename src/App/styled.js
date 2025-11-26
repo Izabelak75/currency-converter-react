53a3125fd029cb6a2e55998ce89edfa5faa5d21b
Izabela Kuc <isabela.kuc@gmail.com>
2025-11-26 00:00:13 -0800

    Updated  App styled.js

--- src/App/styled.js
+++ src/App/styled.js
@@ -2,9 +2,9 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   flex-basis: 700px;
-  margin: 10px;
-  padding: 20px;
-  box-shadow: 4px 4px 30px ${({ theme }) => theme.color.indigo};
+  margin: 20x;
+  padding: 40px;
+  box-shadow: 2x 2px 50px ${({ theme }) => theme.color.indigo};
   border-radius: 10px;
   background: ${({ theme }) => theme.color.white};
 `;
